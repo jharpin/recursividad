48f712d5f39f821b75fadf118f3bc8e1bfaa2905
jharpin <154296932+jharpin@users.noreply.github.com>
2025-09-25 21:22:53 -0500

    permitaciones de rojas

--- Main9Permutaciones.exs
+++ Main9Permutaciones.exs
@@ -1,10 +1,23 @@
-defmodule Permutaciones do
+defmodule Recursividad_permutaciones do
   def generar([]), do: [[]]
+
   def generar(lista) do
-    for x <- lista,
-        resto <- generar(lista -- [x]),
-        do: [x | resto]
+    generar_aux(lista, [])
   end
+
+  defp generar_aux([], _acum), do: []
+  defp generar_aux([x | xs], prev) do
+    resto = prev ++ xs
+
+    permutaciones_resto = generar(resto)
+
+    con_x = agregar(x, permutaciones_resto)
+
+    con_x ++ generar_aux(xs, prev ++ [x])
+  end
+
+  defp agregar(_x, []), do: []
+  defp agregar(x, [head | tail]), do: [[x | head] | agregar(x, tail)]
 end
 
-IO.inspect Permutaciones.generar([1, 2, 3])
+IO.inspect(Recursividad_permutaciones.generar([1, 2, 3, 4]))
